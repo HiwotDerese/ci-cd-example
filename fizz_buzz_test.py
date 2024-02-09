a8dd46c54f41e8dfe42ec5efd168a0d113ceee7c
Hiwot Derese <Hiwotderese16@gmail.com>
2024-02-09 14:27:41 +0300

    add test

---
+++ fizz_buzz_test.py
@@ -0,0 +1,12 @@
+from main import fizzbuzz
+
+def test_fizzbuzz():
+    assert fizzbuzz(3) == "Fizz"
+    # assertEqual(fizzbuzz(5), "Buzz")
+    # assertEqual(fizzbuzz(15), "FizzBuzz")
+    # assertEqual(fizzbuzz(7), 7)
+    # Add more test cases as needed
+
+
+
+    
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 def main():
+    number = input("Please enter a number: ")
+    print(fizzbuzz(int(number)))    
     
 
-    def fizzbuzz(number):
-        if number % 3 == 0 and number % 5 == 0:
-            return "FizzBuzz"
-        elif number % 3 == 0:
-            return "Fizz"
-        elif number % 5 == 0:
-            return "Buzz"
-        else:
-            return number
-    # print("Hello, " + name + "Docker is running")
-    number = input("Please enter a number: ")
-    print(fizzbuzz(int(number)))
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
+# print("Hello, " + name + "Docker is running")
+
 
 if __name__ == "__main__":
     main()
